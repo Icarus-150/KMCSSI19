9b6acb292a9b3bb20ce459790b18da162f26b59f
Kevin Mendoza <mendo329@csusm.edu>
2019-07-24 12:01:27 -0700

    javabegining

---
+++ Welearn/M1-HTML-CSS/LABS/labs 2 format/script.js
@@ -0,0 +1,29 @@
+ console.log("script running");
+ let name = ' kevin';
+ console.log('hello' + name + '!');
+ let age =15;
+ age=18;
+ console.log('you are ' + age + " years old");
+ var randomNumberBetween0and100 = Math.floor(Math.random() * 101);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.random();
+}
+console.log(randomNumberBetween0and100);
+ if (age<=randomNumberBetween0and100 )
+ {
+console.log("damn you a raisin!" );
+ }
+else
+{
+console.log("you need to grow some 'ore'");
+}
+
+const funtionname =(para1,para2 )=>
+{
+
+
+}
---
+++ Welearn/M2-JAVASCRIPT/console/script.js
@@ -0,0 +1,21 @@
+const greetpeople=(name1,name2)=>
+{
+console.log("HELLO " + name1+ "and " + name2 +"! " );
+}
+greetpeople ("padiuxhi ","adam ");
+
+let radius= 10;
+let area = Math.PI*radius*radius;
+radius=radius*2;
+area= Math.PI * Math.pow(radius,2)
+let width=Math.round(Math.random()* 100);
+let height=Math.round(Math.random()* 50);
+circumfrence = width * 2 + height * 2;
+
+let n=1;
+const increasenumber = () =>{
+n=n+1;
+console.log(n);
+
+}
+setInterval(increasenumber,10);
