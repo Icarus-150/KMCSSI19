efc37986ce99ccfebd84b5a8ac4431be9edebeec
Kevin Mendoza <mendo329@csusm.edu>
2019-07-26 10:32:14 -0700

    sum

---
+++ Welearn/M3-PYTHON/L1-PYTHON-INTRO/hello.py
@@ -0,0 +1,64 @@
+"""num1 = int(raw_input("enter number #1:       "))
+num2 = int(raw_input("enter number #2:       "))
+total = num1+num2
+
+print("the total is " + str(total))
+
+print("Mr. stark, i dont feel so good ")
+
+
+
+import time
+num = int(raw_input("enter a number:      "))
+if num> 0:
+    print("thats a positive number!!!!!     ")
+elif num < 0:
+    print("thats a negative number!!!!!!!")
+else:
+    print("0")
+    print("Mr. stark, i dont feel so good ")
+    time.sleep(1)
+    print(" i dont wanna go...... ")
+
+
+const names =['alice','bob','charlie','deborah','evan'];
+
+for(let i = 0; i < 5;i++)
+
+
+for i in range(5)
+    print(i)
+
+x = 0
+while x <= 5:
+    print (x)
+    x=x+1
+
+my_name = "kevin"
+friend1 = "yaneyry"
+friend2 = "zachary"
+friend3 = "sonny (and bean)"
+
+print(
+"my name is %s and mis amigos son %s,%s and %s "%
+(friend1,my_name,friend2,friend3)
+)
+
+def greetagent(firstname ,lastname ):
+    print("%s. %s, %s"%(lastname,firstname,lastname)
+    )
+
+
+
+greetagent("james","bond")
+
+"""
+def findSum(x):
+    sum=0
+    for i in range(0,x+1):
+        sum+=i
+    print(sum)
+
+
+
+findSum(int(raw_input("enter number #1:       ")))
